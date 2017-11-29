7bab5e280cbfd5d6bc1611956d69c990157eced6
Sabine Hartmann <sabine.k.hartmann@gmail.com>
2017-11-29 10:12:55 +0100

    1st commit Basic Exercise 1 + 2

---
+++ FSWD30-JS-Day01_Basic_Exercise.js
@@ -0,0 +1,6 @@
+/* Create an external Javascript program that prints 
+the following message on the screen "Hi my name is Mathias" and 
+link it to HTML document. Once the HTML file is opened, 
+it should display the message from the Javascript.*/
+
+document.write("Hi my name is Mathias");
